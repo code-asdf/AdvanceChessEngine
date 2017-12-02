7d19c776f1456c1b5d559e6e88c25d500d391ba4
bloodseeker98 <aman.goel98@gmail.com>
2017-12-02 20:19:01 +0530

    contains UI for normal as well as chess960

---
+++ src/BoardGeneration.java
@@ -0,0 +1,167 @@
+import java.util.Arrays;
+
+/*
+PIECE = WHITE/black
+pawn=P/p
+knight(horse)=N/n
+bishop=B/b
+rook(castle)=R/r
+Queen = Q/q
+King=K/k
+*/
+public class BoardGeneration {
+    public static void initiateStandardChess(){
+        long WP=0L,WN=0L,WB=0L,WR=0L,WQ=0L,WK=0L,BP=0L,BN=0L,BB=0L,BR=0L,BQ=0L,BK=0L;
+        String chessBoard[][]={
+                {"r","n","b","q","k","b","n","r"},
+                {"p","p","p","p","p","p","p","p"},
+                {" "," "," "," "," "," "," "," "},
+                {" "," "," "," "," "," "," "," "},
+                {" "," "," "," "," "," "," "," "},
+                {" "," "," "," "," "," "," "," "},
+                {"P","P","P","P","P","P","P","P"},
+                {"R","N","B","Q","K","B","N","R"}};
+        arrayToBitboards(chessBoard,WP,WN,WB,WR,WQ,WK,BP,BN,BB,BR,BQ,BK);
+    }
+    public static void initiateChess960(){
+        long WP=0L,WN=0L,WB=0L,WR=0L,WQ=0L,WK=0L,BP=0L,BN=0L,BB=0L,BR=0L,BQ=0L,BK=0L;
+        String chessBoard[][]={
+                {" "," "," "," "," "," "," "," "},
+                {"p","p","p","p","p","p","p","p"},
+                {" "," "," "," "," "," "," "," "},
+                {" "," "," "," "," "," "," "," "},
+                {" "," "," "," "," "," "," "," "},
+                {" "," "," "," "," "," "," "," "},
+                {"P","P","P","P","P","P","P","P"},
+                {" "," "," "," "," "," "," "," "}};
+        //step 1:
+        int random1 =(int)(Math.random()*8);
+        chessBoard[0][random1]="b";
+        chessBoard[7][random1]="B";
+        //step 2:
+        int random2=(int)(Math.random()*8);
+        while(random2%2==random1%2){
+            random2=(int)(Math.random()*8);
+        }
+        chessBoard[0][random2]="b";
+        chessBoard[7][random2]="B";
+        //step 3:
+        int random3 = (int)(Math.random()*8);
+        while(random3==random1 || random3==random2){
+            random3=(int)(Math.random()*8);
+        }
+        chessBoard[0][random3]="q";
+        chessBoard[7][random3]="Q";
+        //step 4:
+        int random4a=(int)(Math.random()*5);
+        int counter=0;
+        int loop=0;
+        while(counter-1<random4a){
+            if(" ".equals(chessBoard[0][loop])){
+                counter++;
+            }
+            loop++;
+        }// did not understand
+        chessBoard[0][loop-1]="n";
+        chessBoard[7][loop-1]="N";
+        int random4b=(int)(Math.random()*4);
+        counter=0;
+        loop=0;
+        while(counter-1<random4b){
+            if(" ".equals(chessBoard[0][loop])){
+                counter++;
+            }
+            loop++;
+        }
+        chessBoard[0][loop-1]="n";
+        chessBoard[7][loop-1]="N";
+        //step 5:
+        counter=0;
+        while(!" ".equals(chessBoard[0][counter])){
+            counter++;
+        }
+        chessBoard[0][counter]="r";
+        chessBoard[7][counter]="R";
+        while(!" ".equals(chessBoard[0][counter])){
+            counter++;
+        }
+        chessBoard[0][counter]="k";
+        chessBoard[7][counter]="K";
+        while(!" ".equals(chessBoard[0][counter])){
+            counter++;
+        }
+        chessBoard[0][counter]="r";
+        chessBoard[7][counter]="R";
+        arrayToBitboards(chessBoard,WP,WN,WB,WR,WQ,WK,BP,BN,BB,BR,BQ,BK);
+    }
+    public static void arrayToBitboards(String[][] chessBoard,long WP,long WN,long WB,long WR,long WQ,long WK,long BP,long BN,long BB,long BR,long BQ,long BK) {
+        String Binary;
+        for(int i=0;i<64;i++){
+            Binary="0000000000000000000000000000000000000000000000000000000000000000";
+            Binary=Binary.substring(i+1)+"1"+Binary.substring(0,i);
+            switch (chessBoard[i/8][i%8]) {
+                case "P": WP+=convertStringToBitboard(Binary);
+                    break;
+                case "N": WN+=convertStringToBitboard(Binary);
+                    break;
+                case "B": WB+=convertStringToBitboard(Binary);
+                    break;
+                case "R": WR+=convertStringToBitboard(Binary);
+                    break;
+                case "Q": WQ+=convertStringToBitboard(Binary);
+                    break;
+                case "K": WK+=convertStringToBitboard(Binary);
+                    break;
+                case "p": BP+=convertStringToBitboard(Binary);
+                    break;
+                case "n": BN+=convertStringToBitboard(Binary);
+                    break;
+                case "b": BB+=convertStringToBitboard(Binary);
+                    break;
+                case "r": BR+=convertStringToBitboard(Binary);
+                    break;
+                case "q": BQ+=convertStringToBitboard(Binary);
+                    break;
+                case "k": BK+=convertStringToBitboard(Binary);
+                    break;
+            }
+        }
+        drawArray(WP,WN,WB,WR,WQ,WK,BP,BN,BB,BR,BQ,BK);
+        UserInterface.WP=WP; UserInterface.WN=WN; UserInterface.WB=WB;
+        UserInterface.WR=WR; UserInterface.WQ=WQ; UserInterface.WK=WK;
+        UserInterface.BP=BP; UserInterface.BN=BN; UserInterface.BB=BB;
+        UserInterface.BR=BR; UserInterface.BQ=BQ; UserInterface.BK=BK;
+    }
+    public static  long convertStringToBitboard(String Binary){
+        if(Binary.charAt(0)=='0'){//not going to be a negative number
+            return Long.parseLong(Binary,2);
+        }else{
+            return Long.parseLong("1"+Binary.substring(2),2)*2;
+        }
+    }
+    public static void drawArray(long WP,long WN,long WB,long WR,long WQ,long WK,long BP,long BN,long BB,long BR,long BQ,long BK) {
+        String chessBoard[][] = new String[8][8];
+        for(int i=0;i<64;i++){
+            chessBoard[i/8][i%8]=" ";
+        }
+        for(int i=0;i<64;i++){
+            if (((WP>>i)&1)==1) {chessBoard[i/8][i%8]="P";}
+            if (((WN>>i)&1)==1) {chessBoard[i/8][i%8]="N";}
+            if (((WB>>i)&1)==1) {chessBoard[i/8][i%8]="B";}
+            if (((WR>>i)&1)==1) {chessBoard[i/8][i%8]="R";}
+            if (((WQ>>i)&1)==1) {chessBoard[i/8][i%8]="Q";}
+            if (((WK>>i)&1)==1) {chessBoard[i/8][i%8]="K";}
+            if (((BP>>i)&1)==1) {chessBoard[i/8][i%8]="p";}
+            if (((BN>>i)&1)==1) {chessBoard[i/8][i%8]="n";}
+            if (((BB>>i)&1)==1) {chessBoard[i/8][i%8]="b";}
+            if (((BR>>i)&1)==1) {chessBoard[i/8][i%8]="r";}
+            if (((BQ>>i)&1)==1) {chessBoard[i/8][i%8]="q";}
+            if (((BK>>i)&1)==1) {chessBoard[i/8][i%8]="k";}
+        }
+        for(int i=0;i<8;i++){
+            System.out.println(Arrays.toString(chessBoard[i]));
+        }
+    }
+
+
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class UserInterface extends JPanel {
+    static long WP=0L,WN=0L,WB=0L,WR=0L,WQ=0L,WK=0L,BP=0L,BN=0L,BB=0L,BR=0L,BQ=0L,BK=0L;
+    static long UniversalWP=0L,UniversalWN=0L,UniversalWB=0L,UniversalWR=0L,UniversalWQ=0L,UniversalWK=0L,UniversalBP=0L,UniversalBN=0L,UniversalBB=0L,UniversalBR=0L,UniversalBQ=0L,UniversalBK=0L;
+    static int humanIsWhite=1;
+    static int rating=0;
+    static int border=10;//the amount of empty space around the frame
+    static double squareSize=64;//the size of a chess board square
+    static JFrame javaF=new JFrame("Advance Chess Engine ");//must be declared as static so that other class' can repaint
+    static UserInterface javaUI=new UserInterface();//must be declared as static so that other class' can repaint
+    public static void main(String[] args) {
+        javaF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        javaF.add(javaUI);
+        javaF.setSize(757, 570);
+        javaF.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width-javaF.getWidth())/2,
+                (Toolkit.getDefaultToolkit().getScreenSize().height-javaF.getHeight())/2);
+        javaF.setVisible(true);
+        newGame();
+        javaF.repaint();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(new Color(200, 100, 0));
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                squareSize=(double)(Math.min(getHeight(), getWidth()-200-border)-2*border)/8;
+            }
+        });
+        drawBorders(g);
+        drawBoard(g);
+        drawPieces(g);
+    }
+    public void drawBoard(Graphics g) {
+        for (int i=0;i<64;i+=2) {//draw chess board
+            g.setColor(new Color(255, 200, 100));
+            g.fillRect((int)((i%8+(i/8)%2)*squareSize)+border, (int)((i/8)*squareSize)+border, (int)squareSize, (int)squareSize);
+            g.setColor(new Color(150, 50, 30));
+            g.fillRect((int)(((i+1)%8-((i+1)/8)%2)*squareSize)+border, (int)(((i+1)/8)*squareSize)+border, (int)squareSize, (int)squareSize);
+        }
+    }
+    public void drawPieces(Graphics g) {
+        Image chessPieceImage;
+        chessPieceImage=new ImageIcon("ChessPiecesHD.png").getImage();
+        for (int i=0;i<64;i++) {
+            int j=-1,k=-1;
+            if (((WP>>i)&1)==1) {j=5; k=1-humanIsWhite;}
+            else if (((BP>>i)&1)==1) {j=5; k=humanIsWhite;}
+            else if (((WB>>i)&1)==1) {j=2;k=1-humanIsWhite;}
+            else if (((BB>>i)&1)==1) {j=2;k=humanIsWhite;}
+            else if (((WN>>i)&1)==1) {j=3;k=1-humanIsWhite;}
+            else if (((BN>>i)&1)==1) {j=3;k=humanIsWhite;}
+            else if (((WQ>>i)&1)==1) {j=1;k=1-humanIsWhite;}
+            else if (((BQ>>i)&1)==1) {j=1;k=humanIsWhite;}
+            else if (((WR>>i)&1)==1) {j=4;k=1-humanIsWhite;}
+            else if (((BR>>i)&1)==1) {j=4;k=humanIsWhite;}
+            else if (((WK>>i)&1)==1) {j=0;k=1-humanIsWhite;}
+            else if (((BK>>i)&1)==1) {j=0;k=humanIsWhite;}
+            if (j!=-1 && k!=-1) {
+                g.drawImage(chessPieceImage, (int)((i%8)*squareSize)+border, (int)((i/8)*squareSize)+border, (int)((i%8+1)*squareSize)+border, (int)((i/8+1)*squareSize)+border, j * chessPieceImage.getWidth(this)/6, k * chessPieceImage.getHeight(this)/2, (j + 1) * chessPieceImage.getWidth(this)/6, (k + 1) * chessPieceImage.getHeight(this)/2, this);
+            }
+        }
+    }
+    public void drawBorders(Graphics g) {
+        g.setColor(new Color(100, 0, 0));
+        g.fill3DRect(0, border, border, (int)(8*squareSize), true);
+        g.fill3DRect((int)(8*squareSize)+border, border, border, (int)(8*squareSize), true);
+        g.fill3DRect(border, 0, (int)(8*squareSize), border, true);
+        g.fill3DRect(border, (int)(8*squareSize)+border, (int)(8*squareSize), border, true);
+
+        g.setColor(Color.BLACK);
+        g.fill3DRect(0, 0, border, border, true);
+        g.fill3DRect((int)(8*squareSize)+border, 0, border, border, true);
+        g.fill3DRect(0, (int)(8*squareSize)+border, border, border, true);
+        g.fill3DRect((int)(8*squareSize)+border, (int)(8*squareSize)+border, border, border, true);
+        g.fill3DRect((int)(8*squareSize)+2*border+200, 0, border, border, true);
+        g.fill3DRect((int)(8*squareSize)+2*border+200, (int)(8*squareSize)+border, border, border, true);
+
+        g.setColor(new Color(0,100,0));
+        g.fill3DRect((int)(8*squareSize)+2*border, 0, 200, border, true);
+        g.fill3DRect((int)(8*squareSize)+2*border+200, border, border, (int)(8*squareSize), true);
+        g.fill3DRect((int)(8*squareSize)+2*border, (int)(8*squareSize)+border, 200, border, true);
+    }
+    public static void newGame() {
+        BoardGeneration.initiateStandardChess();
+    }
+}
